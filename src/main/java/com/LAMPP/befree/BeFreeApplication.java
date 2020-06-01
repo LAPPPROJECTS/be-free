0faee8655b11695086e56583867982559900bce2
LukaszLeszczynski <lukaszleszczynski2@gmail.com>
2020-06-02 00:20:01 +0100

    Initial commit

---
+++ src/main/java/com/LAMPP/befree/BeFreeApplication.java
@@ -0,0 +1,13 @@
+package com.LAMPP.befree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LAMPP/befree/BeFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LAMPP.befree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
