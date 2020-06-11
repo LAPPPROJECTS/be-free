cf172d611f5a581cff67ce26910a19f4caf0bb8a
LukaszLeszczynski <lukaszleszczynski2@gmail.com>
2020-06-11 20:50:31 +0100

    [review +rating ] final

--- src/main/java/com/LAMPP/befree/review/Review.java
+++ src/main/java/com/LAMPP/befree/review/Review.java
@@ -25,13 +25,6 @@ public class Review implements Serializable {
 
     }
 
-    //private LocalDateTime with(LocalDate newDate, LocalTime newTime) {
-    //    if (date == newDate && time == newTime) {
-    //        return this.localDateTime;
-    //    }
-    //    return LocalDateTime.now ();
-    //}
-
     public String getContent() {
         return content;
     }
