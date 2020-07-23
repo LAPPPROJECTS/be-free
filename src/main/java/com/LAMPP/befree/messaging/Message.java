0c87ae1a95e7df8026f53676f67adfcaf44e3b96
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-07-23 15:47:54 +0100

    added spring-starter-validation to dependencies first validation of message properties

--- src/main/java/com/LAMPP/befree/messaging/Message.java
+++ src/main/java/com/LAMPP/befree/messaging/Message.java
@@ -4,6 +4,9 @@ package com.LAMPP.befree.messaging;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Entity
@@ -13,8 +16,14 @@ public class Message {
     @Id
     private UUID id;
 
+    @NotNull
     private UUID toProfile;
+
+    @NotNull
     private UUID fromProfile;
+
+    @NotBlank
+    @Max(value = 5000, message = "Message too long! You can send up to 5000 characters" )
     private String body;
 
     public Message(UUID toProfile, UUID fromProfile, String body) {
