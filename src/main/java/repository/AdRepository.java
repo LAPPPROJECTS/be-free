a080c154c37a162dc196a4c87f1cfc82c1011caf
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-07-29 21:16:33 +0100

    poprawione repozytorium -v2

--- src/main/java/repository/AdRepository.java
+++ src/main/java/repository/AdRepository.java
@@ -6,38 +6,29 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class AdRepository {
     public List<Ad> adList = new ArrayList<>();
 
     public AdRepository() {
-        adList.add(new Ad("Nazwa ogloszenia", 1234));
-        adList.add(new Ad("Inna nazwa ogloszenia", 1333));
+        adList.add(new Ad("Tytul", "text", UUID.randomUUID()));
+        adList.add(new Ad("Inna nazwa ogloszenia", "innyText", UUID.randomUUID()));
     }
 
-    public void setAdList(Collection<Object> adList) {
-        // adRepository.adList = adList;
+    public Ad getById(UUID id) {
+        return adList.stream()
+                .filter(ad -> ad.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
 
-    public List<Ad> getAdList() {              //tutaj podpowiada getAdList a poprzednio bylo getAll
-        return adList;
-    }
-    public Ad getById(int id) {
-        return (Ad) adList.stream()
-                    .filter(ad -> ad.getId() == id)
-                    .findFirst()
-                    .orElse(null);
-    }
     public void addAd(Ad ad) {
         adList.add(ad);
     }
 
-    public void setAdList(List<Ad> adList) {
-        this.adList = adList;
-    }
-
     public List<Ad> getAll() {
-        return null;
+        return adList;
     }
 }
