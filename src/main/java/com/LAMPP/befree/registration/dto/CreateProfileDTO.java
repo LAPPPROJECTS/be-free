722467a5e36ec930ba9b2fac40d5d99e82ab0d79
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-08-04 12:46:40 +0100

    added validation messages

--- src/main/java/com/LAMPP/befree/registration/dto/CreateProfileDTO.java
+++ src/main/java/com/LAMPP/befree/registration/dto/CreateProfileDTO.java
@@ -8,11 +8,11 @@ import javax.validation.constraints.Size;
 public class CreateProfileDTO {
 
     @NotBlank(message = "Please provide a name")
-    @Size(min = 3)
+    @Size(min = 3, message = "Name must be longer than 3 characters")
     public String name;
 
     @NotBlank(message = "Please provide a surname")
-    @Size(min = 3)
+    @Size(min = 3, message = "Surname must be longer than 3 characters")
     public String surname;
 
     @NotBlank(message = "Email is required")
@@ -25,11 +25,13 @@ public class CreateProfileDTO {
 
     @NotBlank(message = "Please enter your password")
     @Size(min = 8, max = 15, message = "Your password must have between 8 and 15 characters")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@#$%^&*])(?=\\S+$).{8,15}$")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@#$%^&*])(?=\\S+$).{8,15}$",
+            message = "Password must be between 8 and 15 characters, contain at least one digit," +
+                    "at least one lower case, at least one upper case and one special character")
     public String password;
 
     @NotBlank(message = "Phone number is required")
-    @Size(min = 11, max = 11)
+    @Size(min = 11, max = 11, message = "Phone number not valid")
     public String phoneNumber;
 
     public int age;
